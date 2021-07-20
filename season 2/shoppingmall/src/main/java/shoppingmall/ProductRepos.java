e94855403ebb48ed8685c3f52780de5e01312cb8
Moon-ki Kim <mkmkqoa@gmail.com>
2021-07-20 20:21:20 +0900

    Add products

--- season 2/shoppingmall/src/main/java/shoppingmall/ProductRepos.java
+++ season 2/shoppingmall/src/main/java/shoppingmall/ProductRepos.java
@@ -26,6 +26,91 @@ public class ProductRepos {
         data3.add("100000");
         data3.add("100");
 
+        ArrayList<String> data4 = new ArrayList<>();
+        data3.add("BB크림");
+        data3.add("30000");
+        data3.add("100");
+
+        ArrayList<String> data5 = new ArrayList<>();
+        data3.add("향긋 샴푸");
+        data3.add("15000");
+        data3.add("100");
+
+        ArrayList<String> data6 = new ArrayList<>();
+        data3.add("매니큐어");
+        data3.add("11000");
+        data3.add("100");
+
+        ArrayList<String> data7 = new ArrayList<>();
+        data3.add("저렴 TV");
+        data3.add("709000");
+        data3.add("100");
+
+        ArrayList<String> data8 = new ArrayList<>();
+        data3.add("세탁기");
+        data3.add("360000");
+        data3.add("100");
+
+        ArrayList<String> data9 = new ArrayList<>();
+        data3.add("공유기");
+        data3.add("59000");
+        data3.add("100");
+
+        ArrayList<String> data10 = new ArrayList<>();
+        data3.add("식탁");
+        data3.add("79000");
+        data3.add("100");
+
+        ArrayList<String> data11 = new ArrayList<>();
+        data3.add("암막커튼");
+        data3.add("26000");
+        data3.add("100");
+
+        ArrayList<String> data12 = new ArrayList<>();
+        data3.add("책꽂이");
+        data3.add("108000");
+        data3.add("100");
+
+        ArrayList<String> data13 = new ArrayList<>();
+        data3.add("기저귀");
+        data3.add("16900");
+        data3.add("100");
+
+        ArrayList<String> data14 = new ArrayList<>();
+        data3.add("유모차");
+        data3.add("398000");
+        data3.add("100");
+
+        ArrayList<String> data15 = new ArrayList<>();
+        data3.add("햅쌀");
+        data3.add("38000");
+        data3.add("100");
+
+        ArrayList<String> data16 = new ArrayList<>();
+        data3.add("은갈치");
+        data3.add("33000");
+        data3.add("100");
+
+        ArrayList<String> data17 = new ArrayList<>();
+        data3.add("흑당버블티 아이스바");
+        data3.add("18900");
+        data3.add("100");
+
+        ArrayList<String> data18 = new ArrayList<>();
+        data3.add("종가집 배추김치");
+        data3.add("29900");
+        data3.add("100");
+
+        ArrayList<String> data19 = new ArrayList<>();
+        data3.add("러닝머신");
+        data3.add("1289000");
+        data3.add("100");
+
+        ArrayList<String> data20 = new ArrayList<>();
+        data3.add("이어폰");
+        data3.add("30000");
+        data3.add("100");
+
         productList.add(data1);
         productList.add(data2);
         productList.add(data3);
